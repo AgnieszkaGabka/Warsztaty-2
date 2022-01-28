858a3350c61f7474d8cf67162c9f3a1dbc597abc
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-01-28 12:45:07 +0100

    Added Operational Error override, db name changed from Users to users

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
-from psycopg2 import connect
-from psycopg2.errors import DuplicateDatabase
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
 
-sql1 = """CREATE DATABASE Users;"""
+sql1 = """CREATE DATABASE users;"""
 
 sql2 = """CREATE TABLE users
 (
@@ -27,13 +27,46 @@ USER = "postgres"
 HOST = "localhost"
 PASSWORD = "coderslab"
 
-cnx = connect(user=USER, host=HOST, password=PASSWORD)
-cnx.autocommit = True
-cursor = cnx.cursor()
 try:
-    cursor.execute(sql1)
-    print("Database Users created")
-except DuplicateDatabase:
-    print("Database already created")
+    cnx = connect(user=USER, host=HOST, password=PASSWORD)
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+    try:
+        cursor.execute(sql1)
+        print("Database users created")
+    except DuplicateDatabase:
+        print("Database already created")
+except OperationalError:
+    print ("Operational Error")
+
+cnx.close()
+
+try:
+    cnx = connect(user=USER, host=HOST, password=PASSWORD, database="users")
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute(sql2)
+        print("Table users created")
+    except DuplicateTable:
+        print("Table users already created")
+except OperationalError:
+    print ("Operational Error")
+
+cnx.close()
+
+try:
+    cnx = connect(user=USER, host=HOST, password=PASSWORD, database="users")
+    cnx.autocommit = True
+    cursor = cnx.cursor()
+
+    try:
+        cursor.execute(sql3)
+        print("Table messages created")
+    except DuplicateTable:
+        print("Table messages already created")
+except OperationalError:
+    print ("Operational Error")
 
 cnx.close()
