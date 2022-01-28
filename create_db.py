69718adc745a8e6644d4e8c5306f06ca48bf788a
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-01-28 14:02:48 +0100

    Added class Message, its properties and staticmethods

--- create_db.py
+++ create_db.py
@@ -156,3 +156,50 @@ class User
         self._id = -1
         return True
 
+class Message
+    def __init__(self, from_id, to_id, text):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self.creation_date = None
+
+    @property
+    def id(self):
+        return self._id
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO Messages(from_id, to_id, text)
+                            VALUES(%s, %s, %s) RETURNING id, creation_date"""
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()['id']
+            self.creation_date = cursor.fetchone()['timestamp']
+            return True
+        else:
+            sql = """UPDATE Users SET from_id=%s, to_id=%s, text=%s
+                           WHERE id=%s"""
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            return True
+
+    @staticmethod
+    def load_all_messages(cursor, user_id=None):
+        messages = []
+        if user_id is not None:
+            sql = "SELECT id, from_id, to_id, text, FROM Message"
+            cursor.execute(sql, (user_id,))
+        else:
+            sql = "SELECT id, from_id, to_id, text, FROM Message"
+            cursor.execute(sql)
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Message(from_id, to_id, text)
+            loaded_message._id = id_
+            loaded_message.from_id = from_id
+            loaded_message.to_id = to_id
+            loaded_message._creation_date = creation_date
+            loaded_message.text = text
+            messages.append(loaded_message)
+        return messages
