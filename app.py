d65b6cf8ee5d13250c9dca9eea8a7a2b45a0e991
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-01-30 18:34:17 +0100

    Created functional app with argparse

--- app.py
+++ app.py
@@ -71,3 +71,66 @@ if __name__ == '__main__':
         cnx.close()
     except OperationalError:
         print("Operational Error")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--username", help="username")
+parser.add_argument("-p", "--password", help="password - min 8 characters")
+parser.add_argument("-t", "--to", help="message receiver")
+parser.add_argument("-s", "--send", help="message text")
+parser.add_argument("-l", "--list", help="list users", action="store_true")
+
+args = parser.parse_args()
+
+def list_messages(cursor, username, password, list):
+    user = User.load_user_by_username(cursor, username)
+    if not user:
+        print("No such user")
+    elif check_password(password, user.hashed_password):
+        messages = Message.load_all_messages(cursor, user.id)
+        for message in messages:
+            from_ = User.load_user_by_id(cursor, message.from_id)
+            print(f"from: {from_.username}")
+            print(message.text)
+            print(f"data: {message.creation_date}")
+    else:
+        print("Incorrect password")
+
+def send_message(cursor, username, password, to, send):
+    user = User.load_user_by_username(cursor, username)
+    if not user:
+        print("No such user")
+    elif check_password(password, user.hashed_password):
+        receiver = User.load_user_by_username(cursor, to)
+        if not receiver:
+            print("No such receiver")
+        else:
+            if len(send) > 255:
+                print("Message too long")
+                return
+            else:
+                message = Message(from_id=username, to_id=to, text=send)
+                message.save_to_db(cursor)
+                print("Message send")
+
+if __name__ == '__main__':
+    try:
+        cnx = connect(database="workshop", user="postgres", password="coderslab", host="127.0.0.1")
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        if args.username and args.password:
+            user = User.load_user_by_username(cursor, args.username)
+            if check_password(args.password, user.hashed_password):
+                if args.list:
+                    print_user_messages(cursor, user)
+                elif args.to and args.send:
+                    send_message(cursor, user.id, args.to, args.send)
+                else:
+                    parser.print_help()
+            else:
+                print("Incorrect password or User does not exists!")
+        else:
+            print("username and password are required")
+            parser.print_help()
+        cnx.close()
+    except OperationalError as err:
+        print("Connection Error: ", err)
